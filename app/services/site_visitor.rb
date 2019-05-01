c83774cfb2467631f54daf6ea60bb200125425bf
Jay Kelly <jay4kelly@gmail.com>
2019-05-01 12:06:22 -0400

    making it look pretty v6

--- app/services/site_visitor.rb
+++ app/services/site_visitor.rb
@@ -36,7 +36,6 @@ def print_lifts_open()
   #find('#uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.cuG6ob').text
     #uid_3 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div > div.ByaOAe.wdAYCf > div:nth-child(2) > div.cuG6ob').text
     #uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.cuG6ob').text
-
   end
 def print_summit_depth()
 #depthBaseSummit = find('div.ByaOAe DNjjCd NuspYb').text
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/sugarloaf'
-  get 'welcome/mountSunapee'
-  get 'welcome/patsPeak'
   get 'welcome/sugarbush'
-  get 'welcome/blueHillsSkiArea'
-  get 'welcome/mountAbram'
-  get 'welcome/wachusettMountain'
   get 'welcome/sundayRiver'
-  get 'welcome/berkshireEast'
   get 'welcome/newengland'
   get 'welcome/index'
-
-
 resources :reviews do
   resources :comments, only: [:index, :new, :create]
 end
 resources :comments, only: [:show, :edit, :update, :destroy]
 resources :welcome
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome#sugarbush'
+root 'welcome#index'
 end

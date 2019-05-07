bcc67411858120875e42111cfaa566bd4d989cd1
Jay Kelly <jay4kelly@gmail.com>
2019-05-06 20:27:48 -0400

    making it look pretty and fixes v9

--- app/services/site_visitor.rb
+++ app/services/site_visitor.rb
@@ -19,46 +19,66 @@ end
 browser = Capybara.current_session
 driver = browser.driver.browser
 #visit "https://www.google.com"
+begin
+#RestClient::Request.execute(:method => visit :get, :url => "http://www.google.com/search?q=#{@mountain_name}", :timeout => 10, :open_timeout => 10)
 visit "http://www.google.com/search?q=#{@mountain_name}"
+wait = Selenium::WebDriver::Wait.new(:timeout => 5)
 #find(".RNNXgb").click
 #fill_in('q', :with => @mountain).native.send_keys(:return)
 sleep(2)
+rescue Net::HTTPBadResponse
+    false
+end
 end
 
+
 def print_trails_open()
-  return all('.cuG6ob')[1].try(&:text) || "An error has occured"
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+  trails = wait.until{all('.cuG6ob')[1].try(&:text) || "An error has occured"}
+  return trails
 end
 
 def print_lifts_open()
-
-  return all('.cuG6ob')[2].try(&:text) || "An error has occured"
-
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+  lifts = wait.until{all('.cuG6ob')[2].try(&:text) || "An error has occured"}
+  return lifts
+  #return all('.cuG6ob')[2].try(&:text) || "An error has occured"
   #find('#uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.cuG6ob').text
     #uid_3 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div > div.ByaOAe.wdAYCf > div:nth-child(2) > div.cuG6ob').text
     #uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.cuG6ob').text
   end
   def print_base_depth()
-  return  all('.cuG6ob')[3].try(&:text) || "An error has occured"
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    basedepth = wait.until{all('.cuG6ob')[3].try(&:text) || "An error has occured"}
+    return basedepth
+  #return  all('.cuG6ob')[3].try(&:text) || "An error has occured"
 
   #return all('#uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(2) > div:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(1)').try(&:text) || "an error has occured"
-
   end
-  def print_base_temp
-  return  all('.cuG6ob')[4].try(&:text)|| "An error has occured"
-
+  def print_base_temp()
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    basetemp = wait.until{all('.cuG6ob')[4].try(&:text) || "An error has occured"}
+    return basetemp
+  #return  all('.cuG6ob')[4].try(&:text)|| "An error has occured"
 
   end
 def print_summit_depth()
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5)
 #depthBaseSummit = find('div.ByaOAe DNjjCd NuspYb').text
-return  all('.cuG6ob')[5].try(&:text) || "An error has occured"
+summitdepth= wait.until{all('.cuG6ob')[5].try(&:text) || "An error has occured"}
+return summitdepth
+#return  all('.cuG6ob')[5].try(&:text) || "An error has occured"
 
 #all('#uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(2) > div:nth-child(1) > div > table > tbody > tr:nth-child(1) > td:nth-child(1)').try(&:text) || "an error has occured"
 
 end
 
 
-def print_summit_temp
-return  all('.cuG6ob')[6].try(&:text) || "An error has occured"
+def print_summit_temp()
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+  summittemp = wait.until{all('.cuG6ob')[6].try(&:text) || "An error has occured"}
+  return summittemp
+#return  all('.cuG6ob')[6].try(&:text) || "An error has occured"
 
 #return all('#uid_0 > div.ifM9O > div:nth-child(2) > div > div > div > div.wo5SM > div:nth-child(2) > div:nth-child(2) > div > table > tbody > tr:nth-child(1) > td:nth-child(3)').try(&:text) || "An error has occured"
 end
